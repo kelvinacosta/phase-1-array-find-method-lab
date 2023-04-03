2d424cbd914fa9f8d457ad11fccb2e794430dd1d
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-04-02 23:26:00 -0400

    complete function find

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    const myRecord = record.find(game => game.result==='W')
+    if(myRecord){
+        return myRecord.year
+    }
+    
+}
